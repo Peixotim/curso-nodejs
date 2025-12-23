a5b0f1a5970ab6c156834bcffa722d92bde1dea6
Peixotim <pedropeixotovz@gmail.com>
2025-12-23 20:15:25 -0300

    feat: Adding new routes

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ app.get('/health' , (req,res) =>{
     message:`Server is live !`
   })
 })
+
+
+//Route Params
+app.get('/user/:name' , (req,res) => {
+  const {name} = req.params;
+
+  if(!name){
+    res.json({
+      message:`Hello user !`
+    })
+  }
+
+  res.json({
+    message:`Hello your name is ${name}`
+  })
+})
+
+
+ app.get('/user' , (req,res)  =>{
+  const {name, age} = req.query;
+  res.json({
+      message:`Hello ${name} , your age is ${age}`
+  })
+})
