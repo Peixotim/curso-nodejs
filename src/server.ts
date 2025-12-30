c70acf5f8444f9db590e2f2c6c92f0e17998edc3
Peixotim <pedropeixotovz@gmail.com>
2025-12-30 09:43:30 -0300

    refactor: Remove comentário desnecessário na função de bootstrap

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,7 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap(){
   try{
-    await connectMongo(); //Conecao com meu banco de dados mongo
-  
+    await connectMongo(); //Espera a conexao com o banco de dados, la coloquei logs para falar se deu tudo certo ou errado
     app.listen(port,()=>{console.log(`🚀 Server running on port ${port}`)});
   }catch(error){
     console.error('❌ Server failed to start');
