233bc7a23f16c26575dd4cfa9ee86acb1113c949
Peixotim <pedropeixotovz@gmail.com>
2025-12-30 10:11:57 -0300

    feat: Adiciona classes de erro BadRequestError, NotFoundError e UnauthorizedError

---
+++ src/shared/errors/errors.ts
@@ -0,0 +1,19 @@
+import { AppError } from "./AppError.js"
+
+export class BadRequestError extends AppError {
+  constructor(message = 'Bad request') {
+    super(message, 400)
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = 'Not found') {
+    super(message, 404)
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = 'Unauthorized') {
+    super(message, 401)
+  }
+}
