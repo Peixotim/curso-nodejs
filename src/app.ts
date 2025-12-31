e4aa833cdc415bb4ecdf8af7c8da239648d51aac
Peixotim <pedropeixotovz@gmail.com>
2025-12-31 08:44:08 -0300

    feat: Organiza importações e rotas no arquivo app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-
+import trainerRoutes from "./modules/trainer/trainer.routes"
+import pokemonRoutes from "./modules/pokemon/pokemon.routes"
 const app = express()
 
 app.use(express.json())
-
+app.use("/trainers",trainerRoutes)
+app.use("/pokemons",pokemonRoutes)
 export default app;
